4cd271ca2b51db4ae4b5943c1bdf3daf2cc2eca5
gcg <guneycan@gmail.com>
2022-10-17 13:45:50 +0300

    Complete from ALL buffers

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -111,7 +111,14 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
-		{ name = "buffer" },
+		{
+			name = "buffer",
+			option = {
+				get_bufnrs = function()
+					return vim.api.nvim_list_bufs()
+				end,
+			},
+		},
 		{ name = "path" },
 		{ name = "luasnip" },
 	},
