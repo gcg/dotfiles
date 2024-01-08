95b9d45fa20649963663d20e5d8e448c7abafb34
Guney Can Gokoglu <guneycan@gmail.com>
2024-01-08 11:35:44 +0300

    Updated wezterm config with better stuff

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require("wezterm")
-
 local config = {}
 
 if wezterm.config_builder then
@@ -7,28 +6,13 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font({ family = "SauceCodePro NF", weight = "Medium" })
-config.font_size = 12
-config.color_scheme = "Monokai (terminal.sexy)"
---config.color_scheme = "MonokaiPro (Gogh)"
+config.font_size = 14
+config.color_scheme = "Monokai (terminal.sexy)" -- yeah, we do be sexy <3
+--config.color_scheme = "MonokaiPro (Gogh)" -- also as good as the other one, can be replaceable
 
-config.use_fancy_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-
-config.window_frame = {
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#333333",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = "#333333",
-}
-
-config.colors = {
-	tab_bar = {
-		-- The color of the inactive tab bar edge/divider
-		inactive_tab_edge = "#575757",
-	},
-}
+config.use_fancy_tab_bar = true -- i mean, its not that fancy.
+config.hide_tab_bar_if_only_one_tab = false -- dont hide the tab bar since we need it for window buttons
+config.adjust_window_size_when_changing_font_size = false -- dont update window size when adjusting the font size
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE" -- merge window title bar with the tab bar so we save on space.
 
 return config
