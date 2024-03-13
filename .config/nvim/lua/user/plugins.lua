b04b1105106710e621bfc411b6a7fc8d9a120860
Guney Can Gokoglu <guneycan@gmail.com>
2024-03-13 12:25:12 +0300

    added silicon config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -238,6 +238,9 @@ return packer.startup(function(use)
 	use("stevearc/aerial.nvim")
 	use("zk-org/zk-nvim")
 
+	-- create code snapshots
+	use("michaelrommel/nvim-silicon")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
