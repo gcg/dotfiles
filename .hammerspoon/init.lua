c76c36a8d18c88f6cf9b56ce2e174ed96b2404dd
Guney Can Gokoglu <guneycan@gmail.com>
2024-05-04 19:23:22 +0300

    more comments

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,52 +1,5 @@
-function reloadConfig(files)
-	doReload = false
-	for _, file in pairs(files) do
-		if file:sub(-4) == ".lua" then
-			doReload = true
-		end
-	end
-	if doReload then
-		hs.reload()
-	end
-end
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+-- watches config files so we don't have to manually reload
+local ConfigReloader = require("configwatcher")
 
--- start a timer to change the wallpaper
---
-wallpaperDirectory = os.getenv("HOME") .. "/Pictures/Wallpapers"
-wallpapers = {}
--- find and pick a random picture from our wallpaper directory
-function buildWallpapers()
-	hs.alert.show("Building wallpapers")
-
-	wallpapers = {}
-	i = 0
-	for file in hs.fs.dir(wallpaperDirectory) do
-		wallpapers[i] = file
-		i = i + 1
-	end
-	hs.alert.show("Wallpaper table built")
-end
-
--- so we can refresh our table when a new picture comes
-wallpaperWatcher = hs.pathwatcher.new(wallpaperDirectory, buildWallpapers):start()
-
-function updateWallpaper()
-	-- check to see if we have wallpapers loaded
-	wc = #wallpapers
-	if wc < 1 then
-		buildWallpapers()
-	end
-
-	local ri = math.random(#wallpapers)
-	local rwp = wallpapers[ri]
-
-	screen = hs.screen.mainScreen()
-	screen:desktopImageURL("file://" .. wallpaperDirectory .. "/" .. rwp)
-
-	hs.alert.show("Updating wallpaper with: " .. rwp)
-end
-
-wallpaperUpdater = hs.timer.new(300, updateWallpaper)
-wallpaperUpdater:start()
+-- changes wallpaper randomly on all screens in timer
+local WallpaperChanger = require("wallpaper")
---
+++ .hammerspoon/wallpaper.lua
@@ -0,0 +1,40 @@
+-- Select the folder that you store your wallpapers
+wallpaperDirectory = os.getenv("HOME") .. "/Pictures/Wallpapers"
+wallpapers = {}
+-- find and pick a random picture from our wallpaper directory
+function buildWallpapers()
+	hs.alert.show("Building wallpapers")
+	wallpapers = {}
+	i = 0
+	for file in hs.fs.dir(wallpaperDirectory) do
+		wallpapers[i] = file
+		i = i + 1
+	end
+	--hs.alert.show("Wallpaper table built")
+end
+
+-- so we can refresh our table when a new picture comes
+wallpaperWatcher = hs.pathwatcher.new(wallpaperDirectory, buildWallpapers):start()
+
+function updateWallpaper()
+	-- check to see if we have wallpapers loaded
+	wc = #wallpapers
+	if wc < 1 then
+		buildWallpapers()
+	end
+
+	local ri = math.random(#wallpapers)
+	local rwp = wallpapers[ri]
+
+	screen = hs.screen.mainScreen()
+	screen:desktopImageURL("file://" .. wallpaperDirectory .. "/" .. rwp)
+	-- TODO: improve this to change all screens instead of main one
+
+	--hs.alert.show("Updating wallpaper with: " .. rwp)
+end
+
+-- timer config for wallpaper change in seconds
+wallpaperUpdater = hs.timer.new(30, updateWallpaper)
+wallpaperUpdater:start()
+
+-- Random Wallpaper End
