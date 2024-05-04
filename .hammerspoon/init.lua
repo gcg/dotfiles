8a2e542785ead55ce7db6d33f05cd45f06b1e97e
Guney Can Gokoglu <guneycan@gmail.com>
2024-05-04 19:37:01 +0300

    whitespace

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,4 @@
 -- watches config files so we don't have to manually reload
 local ConfigReloader = require("configwatcher")
-
 -- changes wallpaper randomly on all screens in timer
 local WallpaperChanger = require("wallpaper")
