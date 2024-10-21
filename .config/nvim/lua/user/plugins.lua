e6eb598d09fffaf2a6417fe03b5daff1efa51498
Guney Can Gokoglu <guneycan@gmail.com>
2024-10-21 09:38:07 +0300

    chore: added vim-tmux-navigator as a vim plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -254,6 +254,10 @@ return packer.startup(function(use)
 		end,
 	})
 
+	use({
+		"christoomey/vim-tmux-navigator",
+	})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
