df7967c9b59d6dc109f4a5eb5e2192f3b5f0412a
Guney Can Gokoglu <guneycan@gmail.com>
2024-12-16 13:51:24 +0300

    feat: updated terminal font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font({ family = "SauceCodePro NF", weight = "Medium" })
+config.font = wezterm.font({ family = "SauceCodePro NFM", weight = "Medium" })
 config.font_size = 14
 config.color_scheme = "Monokai (terminal.sexy)" -- yeah, we do be sexy <3
 --config.color_scheme = "MonokaiPro (Gogh)" -- also as good as the other one, can be replaceable
