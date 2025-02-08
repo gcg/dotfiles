9305cfa6c9c22100dfd9613fc6d83e5bd8f3796c
Guney Can Gokoglu <guneycan@gmail.com>
2025-02-08 16:59:30 +0300

    chore: remove files that has their own folders now

--- .config/nvim/lua/user/lsp/settings/psalm.lua
+++
@@ -1,6 +0,0 @@
-local util = require 'lspconfig.util'
-return {
-	root_dir = function (fname)
-		return util.root_pattern("psalm.xml")(fname)
-	end
-}
--- .config/nvim/lua/user/lsp/settings/tailwindcss.lua
+++
@@ -1,6 +0,0 @@
-local util = require 'lspconfig.util'
-return {
-	root_dir = function (fname)
-		return util.root_pattern("tailwind.config.js")(fname)
-	end
-}
