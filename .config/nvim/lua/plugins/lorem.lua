876519a44b0f76272a17f78b06fef5c1851bdb22
Guney Can Gokoglu <guneycan@gmail.com>
2025-02-17 19:11:06 +0300

    feat: introducing lorem plugin to generate lorem text

--- .config/nvim/lua/plugins/lorem.lua
+++ .config/nvim/lua/plugins/lorem.lua
@@ -1,4 +1,11 @@
 return {
 	"derektata/lorem.nvim",
 	cmd = "LoremIpsum",
+	config = function()
+		require("lorem").opts({
+			sentenceLength = "medium",
+			comma_chance = 0.2,
+			max_commas_per_sentence = 2,
+		})
+	end,
 }
