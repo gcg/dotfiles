9d7acf6e44fdf0fc9306a7d070e925c245c7dc55
Guney Can Gokoglu <guneycan@gmail.com>
2025-04-20 21:47:57 +0300

    feat: move nvim tree to it's plugin file and remove all custom config to be more lean

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -24,18 +24,6 @@ return {
 
 	{ "numToStr/Comment.nvim" },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
-
 	{ "JoosepAlviste/nvim-ts-context-commentstring" },
 
 	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
