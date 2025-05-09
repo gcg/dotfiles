a942ec713986a862920fbe62b55d3cd6355c0e88
Guney Can Gokoglu <guneycan@gmail.com>
2025-05-09 12:46:50 +0300

    feat: hide .git

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -6,6 +6,8 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			filters = { custom = { "^.git$" } },
+		})
 	end,
 }
