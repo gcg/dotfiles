6d2c4d40b0959dbb19c653388ad7fb974a726e56
Guney Can Gokoglu <guneycan@gmail.com>
2025-09-04 14:43:50 +0300

    feat: move cursor with style powered by smear

---
+++ .config/nvim/lua/plugins/smear_cursor.lua
@@ -0,0 +1,9 @@
+return {
+	"sphamba/smear-cursor.nvim",
+	opts = {
+		stiffness = 0.5,
+		trailing_stiffness = 0.5,
+		matrix_pixel_threshold = 0.5,
+		legacy_computing_symbols_support = true,
+	},
+}
