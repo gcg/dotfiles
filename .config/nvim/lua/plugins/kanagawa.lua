da162128adec0c34715828817b86642276a34933
Guney Can Gokoglu <guneycan@gmail.com>
2025-12-03 10:58:26 +0200

    feat: remove bg gutter

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -2,6 +2,18 @@ return {
 	"rebelot/kanagawa.nvim",
 	priority = 1000,
 	config = function()
+		require("kanagawa").setup({
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+		})
+
 		vim.cmd([[colorscheme kanagawa-dragon]])
 	end,
 }
