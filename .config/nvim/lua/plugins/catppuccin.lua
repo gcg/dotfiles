8da5b1c6579fc3d33312517a050283692a621385
Guney Can Gokoglu <guneycan@gmail.com>
2025-12-03 09:28:53 +0200

    feat: switch to kanagawa theme

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,6 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd([[colorscheme catppuccin-mocha]])
+		-- vim.cmd([[colorscheme catppuccin-mocha]])
 	end,
 }
---
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,7 @@
+return {
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	config = function()
+		vim.cmd([[colorscheme kanagawa-dragon]])
+	end,
+}
