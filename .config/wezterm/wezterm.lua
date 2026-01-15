82cc331141ca9b90bd58d2586941f3e0cd64cdf0
Guney Can Gokoglu <guneycan@gmail.com>
2026-01-15 12:38:41 +0200

    feat: pimp out wezterm to look and feel more like ghostty.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,16 +5,31 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font({ family = "SauceCodePro NFM", weight = "Medium" })
-config.font_size = 14
-config.color_scheme = "Monokai (terminal.sexy)" -- yeah, we do be sexy <3
+-- config.font = wezterm.font({ family = "SauceCodePro NFM", weight = "Medium" })
+config.font = wezterm.font_with_fallback({
+	{ family = "Myna Nerd Font", weight = "Bold" },
+	"SauceCodePro NFM",
+	"Myna",
+	"SF Pro Text",
+})
+config.font_size = 16
+config.freetype_load_target = "HorizontalLcd"
+config.cell_width = 1.1
+-- config.color_scheme = "Monokai (terminal.sexy)" -- yeah, we do be sexy <3
 --config.color_scheme = "MonokaiPro (Gogh)" -- also as good as the other one, can be replaceable
-
+config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.use_fancy_tab_bar = true -- i mean, its not that fancy.
-config.hide_tab_bar_if_only_one_tab = false -- dont hide the tab bar since we need it for window buttons
+config.hide_tab_bar_if_only_one_tab = true -- dont hide the tab bar since we need it for window buttons
 config.adjust_window_size_when_changing_font_size = false -- dont update window size when adjusting the font size
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE" -- merge window title bar with the tab bar so we save on space.
 
-config.window_background_opacity = 0.97 -- the 8th grader inside me really wants a transparent bg but it is not practical :/
+config.window_background_opacity = 0.99 -- the 8th grader inside me really wants a transparent bg but it is not practical :/
+
+config.window_padding = {
+	top = 30,
+	right = 0,
+	left = 0,
+	bottom = 0,
+}
 
 return config
