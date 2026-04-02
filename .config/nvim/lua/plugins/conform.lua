cbb3bfb17c65742b19fd6f423f3c8cd47bc0c547
Guney Can Gokoglu <guneycan@gmail.com>
2026-04-02 11:20:21 +0300

    feat: switch to conform for formatting for everything besides php

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			yaml = { "yq" },
+			go = { "goimports", "gofumpt" },
+			typescript = { "prettierd" },
+			javascript = { "prettierd" },
+		},
+		default_format_opts = {
+			-- Allow formatting from LSP server if no dedicated formatter is available
+			lsp_format = "fallback",
+		},
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+		},
+	},
+}
