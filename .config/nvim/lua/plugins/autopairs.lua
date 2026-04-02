463a1e6d97330aa14086603d19a5ea12598ce131
Guney Can Gokoglu <guneycan@gmail.com>
2026-04-02 15:33:01 +0300

    fix: make autopairs and cmp play nicer.

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,33 +1,34 @@
 return {
 	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = true,
-	opts = {
-		check_ts = true,
-		ts_config = {
-			lua = { "string", "source" },
-			javascript = { "string", "template_string" },
-			java = false,
-		},
-		disable_filetype = { "TelescopePrompt", "spectre_panel" },
-		fast_wrap = {
-			map = "<M-e>",
-			chars = { "{", "[", "(", '"', "'" },
-			pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-			offset = 0, -- Offset from pattern match
-			end_key = "$",
-			keys = "qwertyuiopzxcvbnmasdfghjkl",
-			check_comma = true,
-			highlight = "PmenuSel",
-			highlight_grey = "LineNr",
-		},
+	dependencies = {
+		"hrsh7th/nvim-cmp",
 	},
-	-- use opts = {} for passing setup options
-	-- this is equivalent to setup({}) function
-	-- config = function()
-	-- 	-- If you want insert `(` after select function or method item
-	-- 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-	-- 	local cmp = require("cmp")
-	-- 	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	-- end,
+	event = "InsertEnter",
+	config = function()
+		local autopairs = require("nvim-autopairs")
+		autopairs.setup({
+			check_ts = true,
+			ts_config = {
+				lua = { "string", "source" },
+				javascript = { "string", "template_string" },
+				java = false,
+			},
+			disable_filetype = { "TelescopePrompt", "spectre_panel" },
+			fast_wrap = {
+				map = "<M-e>",
+				chars = { "{", "[", "(", '"', "'" },
+				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+				offset = 0, -- Offset from pattern match
+				end_key = "$",
+				keys = "qwertyuiopzxcvbnmasdfghjkl",
+				check_comma = true,
+				highlight = "PmenuSel",
+				highlight_grey = "LineNr",
+			},
+		})
+		-- If you want insert `(` after select function or method item
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local cmp = require("cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
