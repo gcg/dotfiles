cd5e8446deb6be8c414774dff38ca96f44da00c2
Guney Can Gokoglu <guneycan@gmail.com>
2026-08-14 13:43:08 +0300

    feat: updating treesitter to main

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main", -- Explicitly switch to the actively maintained branch
 	build = ":TSUpdate",
 	lazy = false,
 	dependencies = {
@@ -7,58 +8,58 @@ return {
 		{
 			"Wansmer/treesj",
 			keys = { "<space>m", "<space>j", "<space>s" },
-			dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+			dependencies = { "nvim-treesitter/nvim-treesitter" },
 			config = function()
-				require("treesj").setup({--[[ your config ]]
-				})
+				require("treesj").setup({})
 			end,
 		},
 	},
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- Standardize your list into ensure_installed
-			ensure_installed = {
-				"lua",
-				"vimdoc",
-				"markdown",
-				"php",
-				"javascript",
-				"rust",
-				"html",
-				"bash",
-				"cmake",
-				"css",
-				"dart",
-				"dockerfile",
-				"go",
-				"graphql",
-				"http",
-				"json",
-				"make",
-				"python",
-				"regex",
-				"ruby",
-				"sql",
-				"typescript",
-				"yaml",
-				"vue",
-				"tsx",
-				"prisma",
-				"markdown_inline",
-			},
+		-- The .configs module and ensure_installed list have been removed.
+		-- Parsers are now installed via the native install() method.
+		require("nvim-treesitter").install({
+			"lua",
+			"vimdoc",
+			"markdown",
+			"php",
+			"javascript",
+			"rust",
+			"html",
+			"bash",
+			"cmake",
+			"css",
+			"dart",
+			"dockerfile",
+			"go",
+			"graphql",
+			"http",
+			"json",
+			"make",
+			"python",
+			"regex",
+			"ruby",
+			"sql",
+			"typescript",
+			"yaml",
+			"vue",
+			"tsx",
+			"prisma",
+			"markdown_inline",
+			"swift", -- Added so it installs successfully on startup without auto_install
+		})
 
-			-- Automatically install missing parsers when entering a buffer
-			auto_install = true,
+		-- Highlight and indent are now handled manually via Neovim autocommands
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				-- pcall safely falls back if a filetype lacks an installed parser
+				pcall(vim.treesitter.start)
+			end,
+		})
 
-			highlight = {
-				enable = true,
-				-- Set to false so standard vim syntax doesn't clash with treesitter
-				additional_vim_regex_highlighting = false,
-			},
-
-			indent = {
-				enable = true, -- Safely enables Treesitter indentation
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
